914986565ea1c54b1bd8bb32c39674b11f2a0760
Lindsay <lindsay.lbm@gmail.com>
2019-09-28 15:43:40 -0500

    buttons returning correct responses

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,18 +1,23 @@
 // GLOBAL VARIABLES ==========================================================================================
 
 // topics = [];
-var keyword = "dog";
-var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=hv1F9gem7bGR4Z4zMeZFirQnyj0iUS30&q=" + keyword + "&limit=10";
+
 
 // FUNCTIONS =================================================================================================
 
 // On click event for each button 
+$(".btn").on("click", function (event) {
+    var keyword = $(this).val();
+    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=hv1F9gem7bGR4Z4zMeZFirQnyj0iUS30&q=" + keyword + "&limit=10";
 
+    // ajax call
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+        var animal = response.data
+        console.log(animal);
+    });
 
-// ajax call
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function (response) {
-    console.log(response);
-});
+})
