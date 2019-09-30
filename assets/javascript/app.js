c5f027e9131e1dc234a356cd2b22cc36b7391991
Lindsay <lindsay.lbm@gmail.com>
2019-09-30 08:34:47 -0500

    gifs starting and stopping on click. flex wrap implemented for gif div

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -101,15 +101,29 @@ $("#find-gif").on("click", function (event) {
     renderButtons();
 
     // On click event for pausing and starting the gifs
-    $(".gif").on("click", function () {
-        var state = $(this).attr("data-state");
-        if (state === "still") {
-            $(this).attr("src", $(this).attr("data-animate"));
-            $(this).attr("data-state", "animate");
-        } else {
-            $(this).attr("src", $(this).attr("data-still"));
-            $(this).attr("data-state", "still");
-        }
-    })
+    // $(".gif").on("click", function () {
+    //     console.log("testing")
+    //     var state = $(this).attr("data-state");
+    //     if (state === "still") {
+    //         $(this).attr("src", $(this).attr("data-animate"));
+    //         $(this).attr("data-state", "animate");
+    //     } else {
+    //         $(this).attr("src", $(this).attr("data-still"));
+    //         $(this).attr("data-state", "still");
+    //     }
+    // })
+
 
 })
+
+$(document).on("click", ".gif", function () {
+    console.log("testing")
+    var state = $(this).attr("data-state");
+    if (state === "still") {
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr("data-state", "animate");
+    } else {
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
+    }
+});
